fe89d71a163b201ec659344c9dc87d039444d9e4
Lauren Saunders <laurensaunders@dhcp26-118.gs.washington.edu>
2018-12-13 16:16:11 -0800

    added inflation correction

--- cluster_exprs_testing.R
+++ cluster_exprs_testing.R
@@ -7,7 +7,7 @@ setwd("~/Dropbox (Cole Trapnell's Lab)/yeast_txnClusters/useful_files/")
 
 cds <- readRDS("deltxn_UMAP_2D_50clust.rds")
 
-
+# plot clusters with overlaid labels 
 tmp.df <- pData(cds) %>%
   group_by(Cluster) %>%
   summarize(median_umap_1 = median(umap_1), median_umap_2 = median(umap_2))
@@ -22,24 +22,40 @@ ggplot(pData(cds), aes(x = umap_1, y = umap_2, color = Cluster)) +
     monocle:::monocle_theme_opts()
 
 # modify data distribution label
-new.cds = newCellDataSet(cds@assayData$exprs,
-                         phenoData = new("AnnotatedDataFrame", pData(cds)),
-                         featureData = new("AnnotatedDataFrame", fData(cds)),
-                         expressionFamily = gaussianff())
+cds = newCellDataSet(
+  log2(exprs(cds)/100),
+  phenoData = new("AnnotatedDataFrame", pData(cds)),
+  featureData = new("AnnotatedDataFrame", fData(cds)),
+  expressionFamily = gaussianff()
+)
 
 # run DE test between 2 groups
-dea_df <- two.set.differential.gene.test(new.cds, 
-                      pData(cds)$Cluster == 11, 
-                      pData(cds)$Cluster == 22, formal = TRUE)
+dea_df = two.set.differential.gene.test(cds, 
+                    pData(cds)$Cluster == 11, 
+                    pData(cds)$Cluster == 22, formal = T)
 
 # check expr
 dea_df %>% filter(higher.expr == "Set 1") %>% 
   select(gene, symbol, set.1.mean.expr, set.2.mean.expr, higher.expr, log2.ratio) %>% head(20)
 
+# look at p-val distribution
+hist(dea_df$p.val)
 
+# assess lambda GC (genomic inflation factor)
+dea_df$test.stat = qchisq(1.0 - dea_df$p.val, df = 1)
 
+lambda.gc = median(dea_df$test.stat) / qchisq(0.5, 1)
+lambda.gc
 
+dea_df$inflation.corrected.p.val = pchisq(dea_df$test.stat / lambda.gc, df = 1, lower.tail = F)
+dea_df$inflation.corrected.q.val = p.adjust(dea_df$inflation.corrected.p.val, method = "fdr")
 
+# number of DEGs with and without correction
+sum(dea_df$q.val < 0.05) / nrow(dea_df)
+sum(dea_df$inflation.corrected.q.val < 0.05) / nrow(dea_df)
+
+# look at top genes
+dea_df %>% filter(dea_df$inflation.corrected.q.val < 0.05) %>% head(20)
 
 
 #### wrapper function for differential gene expression testing (modified from J. Packer)
@@ -50,64 +66,45 @@ two.set.differential.gene.test = function(cds, set.1.filter, set.2.filter, forma
   s1.cds = cds[, set.1.filter]
   s2.cds = cds[, set.2.filter]
   
-  s1.norm.expr = Biobase::exprs(s1.cds)
-  s2.norm.expr = Biobase::exprs(s2.cds)
+  s1.mean = apply(Biobase::exprs(s1.cds), 1, mean)
+  s2.mean = apply(Biobase::exprs(s2.cds), 1, mean)
   
-  s1.mean = apply(s1.norm.expr, 1, mean)
-  s2.mean = apply(s2.norm.expr, 1, mean)
-  
-  #s1.n.umi = apply(Biobase::exprs(s1.cds), 1, sum)
-  #s2.n.umi = apply(Biobase::exprs(s2.cds), 1, sum)
-  
-  higher.expr = ifelse(s1.mean > s2.mean, "Set 1", "Set 2")
-  
-  s1.ratio = s1.mean / s2.mean
-  s2.ratio = s2.mean / s1.mean
-  log2.ratio = ifelse(
-    s1.mean == 0 & s2.mean == 0, 0, ifelse(
-      higher.expr == "Set 1", log2(s1.ratio), log2(s2.ratio)))
-  
-  s1.n.expr = apply(Biobase::exprs(s1.cds), 1, function(x) mean(x >= thresh)) #sum?
-  s2.n.expr = apply(Biobase::exprs(s2.cds), 1, function(x) mean(x >= thresh)) #sum?
-  
-  s1.precision = s1.n.expr / (s1.n.expr + s2.n.expr)
-  s1.recall = s1.n.expr / ncol(s1.cds)
-  s2.precision = s2.n.expr / (s1.n.expr + s2.n.expr)
-  s2.recall = s2.n.expr / ncol(s2.cds)
-  
-  precision = ifelse(higher.expr == "Set 1", s1.precision, s2.precision)
-  recall = ifelse(higher.expr == "Set 1", s1.recall, s2.recall)
-  
-  f.score = 2 * precision * recall / (precision + recall)
+  diff = s1.mean - s2.mean
   
   res = data.frame(
     gene = fData(cds)$id,
     symbol = fData(cds)$gene_short_name,
     set.1.mean.expr = s1.mean,
     set.2.mean.expr = s2.mean,
-    higher.expr = higher.expr,
-    log2.ratio = log2.ratio,
-    precision = precision,
-    recall = recall,
-    f.score = f.score
-  ) %>% arrange(-log2.ratio)
-  
+    diff = diff
+  ) %>% arrange(-diff)
   
   if (formal) {
     pData(cds)$tmp = ifelse(set.1.filter, 1, ifelse(set.2.filter, 2, NA))
     
     cds.subset = cds[, set.1.filter | set.2.filter]
-  
+    
     message("Computing differential expression p-values")
     
     DEG = differentialGeneTest(cds.subset,
-                        fullModelFormulaStr = "~ tmp", cores = cores, relative_expr = FALSE)
+                               fullModelFormulaStr = "~ tmp", cores = cores, relative_expr = FALSE)
     
-    print(head(DEG))
+    DEG$test.stat = qchisq(1.0 - DEG$pval, df = 1)
+    
+    infl = median(qchisq(1.0 - DEG$pval, df = 1))
+    
+    lambda.gc = median(DEG$test.stat) / qchisq(0.5, 1)
+    
+    message(paste("inflation value is", infl))
+    
+    DEG$inflation.corrected.p.val = pchisq(DEG$test.stat / lambda.gc, df = 1, lower.tail = F)
+    DEG$inflation.corrected.q.val = p.adjust(DEG$inflation.corrected.p.val, method = "fdr")
     
     res = inner_join(res,
                      DEG %>% select(gene = id,
-                                    p.val = pval, q.val = qval),
+                                    p.val = pval, q.val = qval, 
+                                    inflation.corrected.p.val,
+                                    inflation.corrected.q.val),
                      by = "gene") %>% arrange(q.val)
     
     pData(cds)$tmp = NULL
